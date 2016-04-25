0ae804c872b3f4b746cdad6e3c938b84910b9b1f
Giovanni Scanferla <scanferlagiovanni@yahoo.it>
2016-04-25 19:14:31 +0200

    Now user can join during a game

--- src/sample/interfaccie/ControllerConsole.java
+++ src/sample/interfaccie/ControllerConsole.java
@@ -1,8 +1,16 @@
 package sample.interfaccie;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /**
  * Created by Giovanni on 25/04/2016.
  */
 public class ControllerConsole {
+
+    @FXML
+    Button btnSend;
+
+
 
 }
